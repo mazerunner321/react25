311923855da321d9b63268144f9a9a59a7529c7a
mazerunner321 <prajwalhg321@gmail.com>
2024-02-14 12:12:42 +0530

    First commit

---
+++ src/Accordion/Accordion.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Accordion() {
+  return <div className="text-4xl text-center">Accordion</div>;
+}
+
+export default Accordion;
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Accordion from "./Accordion/Accordion";
+
+function App() {
+  return (
+    <>
+      <Accordion />
+    </>
+  );
+}
+
+export default App;
