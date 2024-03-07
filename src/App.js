c671949437c974972f1bc95616e76600025bd788
mazerunner321 <prajwalhg321@gmail.com>
2024-03-07 13:09:49 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-// import Accordion from "./Accordion/Accordion";
-// import Hex from "./HEX/Hex";
-// import ImgSlider from "./Slider/ImgSlider";
-// import Star from "./StarRating/Star";
-// import LoadMore from "./LoadProducts/LoadMore";
+import Accordion from "./Accordion/Accordion";
+import Hex from "./HEX/Hex";
+import ImgSlider from "./Slider/ImgSlider";
+import Star from "./StarRating/Star";
+import LoadMore from "./LoadProducts/LoadMore";
 import ScrollIndicator from "./Scrollindicator/ScrollIndicator";
-// import ChangeTheme from "./ThemeChange/ChangeTheme";
-// import TreeView from "./TreeView/TreeView";
+import ChangeTheme from "./ThemeChange/ChangeTheme";
+import TreeView from "./TreeView/TreeView";
 
 function App() {
   return (
     <>
-      {/* <Accordion /> */}
-      {/* <Hex /> */}
-      {/* <Star /> */}
-      {/* <ImgSlider /> */}
-      {/* <LoadMore /> */}
-      {/* <TreeView /> */}
-      {/* <ChangeTheme /> */}
+      <Accordion />
+      <Hex />
+      <Star />
+      <ImgSlider />
+      <LoadMore />
+      <TreeView />
+      <ChangeTheme />
       <ScrollIndicator />
     </>
   );
